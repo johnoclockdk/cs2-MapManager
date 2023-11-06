be2029470b96a73c7fd46cce3e4b7870c532786e
johnoclockdk <john.nielsen96dk@gmail.com>
2023-11-06 22:53:18 +0100

    first upload
    upload files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# cs2 MapManager
- allow the usege og multi maps
+# Map Manager
+## ABOUT THE PLUGIN
+With the 'Map Manager' plugin installed, and running on your server. 
+Players will then be allowed to use multiply maps by inputing mapid from steam workshop.
+
+
+## Requirements
+In order for the plugin to work, you must have the following installed:
+- [CounterStrikeSharp](https://docs.cssharp.dev/guides/getting-started/) 
+
+
+## Installation
+1) Download the contents and open the downloaded zip file.
+2) Drag the files within the 'Gameserver' folder into your server's csgo/ directory.
+3) Restart your server, or change the map.
+
+
+## Known Bugs & Issues
+- None.
+
+
+## Bug Reports, Problems & Help
+This plugin has been tested and used on a server, there should be no bugs or issues aside from the known ones found here.
+Should you run into a bug that isn't listed here, then please report it by creating an issue here on GitHub.
---
+++ src/MapManager.cs
@@ -0,0 +1,52 @@
+﻿namespace MapManager;
+
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+public class PluginInfo : BasePlugin
+{
+    public override string ModuleName => "MapManager";
+    public override string ModuleVersion => "0.1";
+    public override string ModuleAuthor => "johnoclock";
+    public override string ModuleDescription => "Allows the usage of more workshop maps.";
+
+
+    public override void Load(bool hotReload)
+    {
+        string filePath = Path.Combine(ModuleDirectory, "MapList.txt");
+
+        if (!File.Exists(filePath))
+            File.Create(filePath);
+
+        RegisterEventHandler<EventCsIntermission>(EventGameEnd);
+        Console.WriteLine("MapManager is loaded");
+    }
+
+
+    private HookResult EventGameEnd(EventCsIntermission @event, GameEventInfo info)
+    {
+        Console.WriteLine("Game has ended. Changing map...");
+
+        // Combine the directory and file name to form the full file path
+        string filePath = Path.Combine(ModuleDirectory, "MapList.txt");
+
+        // Read all lines from MapList.txt into an array
+        string[] mapIdArray = File.ReadAllLines(filePath);
+
+        // Check if there are any IDs in the file
+        if (mapIdArray.Length == 0)
+        {
+            Console.WriteLine("No map IDs found in file.");
+            return HookResult.Continue; // Or however you wish to handle this case
+        }
+
+        // Use a random number generator to pick a random ID from the array
+        Random rng = new Random();
+        string randomMapId = mapIdArray[rng.Next(mapIdArray.Length)]; // Get a random map ID
+
+        // Use the random map ID in the server command
+        Server.ExecuteCommand($"host_workshop_map {randomMapId}");
+
+        return HookResult.Continue;
+    }
+}
