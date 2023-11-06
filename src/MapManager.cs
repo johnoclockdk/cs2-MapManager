58dffe82edfb8a50f0c0d567b98b51f0ddfc4518
johnoclockdk <john.nielsen96dk@gmail.com>
2023-11-06 23:36:46 +0100

    update 0.2
    move the path to global scope:

--- src/MapManager.cs
+++ src/MapManager.cs
@@ -6,17 +6,17 @@ using CounterStrikeSharp.API.Core;
 public class PluginInfo : BasePlugin
 {
     public override string ModuleName => "MapManager";
-    public override string ModuleVersion => "0.1";
+    public override string ModuleVersion => "0.2";
     public override string ModuleAuthor => "johnoclock";
     public override string ModuleDescription => "Allows the usage of more workshop maps.";
 
+    private string FilePath => Path.Join(ModuleDirectory, "MapList.txt");
 
     public override void Load(bool hotReload)
     {
-        string filePath = Path.Combine(ModuleDirectory, "MapList.txt");
 
-        if (!File.Exists(filePath))
-            File.Create(filePath);
+        if (!File.Exists(FilePath))
+            File.Create(FilePath);
 
         RegisterEventHandler<EventCsIntermission>(EventGameEnd);
         Console.WriteLine("MapManager is loaded");
@@ -27,11 +27,8 @@ public class PluginInfo : BasePlugin
     {
         Console.WriteLine("Game has ended. Changing map...");
 
-        // Combine the directory and file name to form the full file path
-        string filePath = Path.Combine(ModuleDirectory, "MapList.txt");
-
         // Read all lines from MapList.txt into an array
-        string[] mapIdArray = File.ReadAllLines(filePath);
+        string[] mapIdArray = File.ReadAllLines(FilePath);
 
         // Check if there are any IDs in the file
         if (mapIdArray.Length == 0)
@@ -41,8 +38,7 @@ public class PluginInfo : BasePlugin
         }
 
         // Use a random number generator to pick a random ID from the array
-        Random rng = new Random();
-        string randomMapId = mapIdArray[rng.Next(mapIdArray.Length)]; // Get a random map ID
+        string randomMapId = mapIdArray[new Random().Next(mapIdArray.Length)]; // Get a random map ID
 
         // Use the random map ID in the server command
         Server.ExecuteCommand($"host_workshop_map {randomMapId}");
